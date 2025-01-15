eab44039fea4d2c78363db2c629094dbd2ca4d83
Rafael Kozar <rafael.kozar@alta-rt.com>
2025-01-15 16:16:14 -0300

    road with lines

--- road.js
+++ road.js
@@ -15,14 +15,16 @@ class Road{
         ctx.lineWidth=5;
         ctx.strokeStyle="white";
 
-        ctx.beginPath();
-        ctx.moveTo(this.left, this.top);
-        ctx.lineTo(this.left, this.bottom);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(this.right, this.top);
-        ctx.lineTo(this.right, this.bottom);    
-        ctx.stroke();
+        for(let i=0; i <= this.laneCount; i++){
+            ctx.beginPath();
+            const x = lerp(this.left, this.right, i/this.laneCount);
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.bottom);
+            ctx.stroke();
+        }
     }
 }
+
+function lerp(a, b, t){
+    return a + (b-a)*t;
+}   
