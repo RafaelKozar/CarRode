636892b045c6d727ac2b904714fd21f19f773a7c
Rafael Kozar <rafael.kozar@alta-rt.com>
2025-01-15 15:57:23 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+myCanvas.height = window.innerHeight;
+myCanvas.width = 200; 
+
+const ctx = myCanvas.getContext('2d');
+//const road =    
+const car = new Car(100, 100, 30, 50);
+
+
+animate();
+
+function animate(){
+    myCanvas.height = window.innerHeight;
+    car.update();
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
